07768be5b011d6a62cd6fbd9b1300c7f1d08ab62
erkike <e.sanchezpo@gmail.com>
2017-02-21 10:42:20 +0100

    sección de comentarios

---
+++ src/main/java/com/series/carpincho/Comentario.java
@@ -0,0 +1,23 @@
+package com.series.carpincho;
+
+public class Comentario {
+	
+	private String texto;
+	
+	public Comentario(){
+		
+	}
+	
+	public Comentario(String texto){
+			this.texto = texto;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+			this.texto = texto;
+	}
+
+}
