d543c908f2bfae4fcb0b4517fefb9a9435338363
erkike <e.sanchezpo@gmail.com>
2017-03-10 17:19:46 +0100

    paginacion y ordenacion

--- src/main/java/com/series/carpincho/Inicio_Controller.java
+++ src/main/java/com/series/carpincho/Inicio_Controller.java
@@ -3,6 +3,9 @@ package com.series.carpincho;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,56 +31,71 @@ public class Inicio_Controller {
 	public void init() {
 		String descripcion = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis euismod pretium. Sed aliquet risus sed ante laoreet luctus. In dapibus massa eu mauris euismod gravida. Donec tempus, orci eu tempor viverra, ex metus vulputate leo, et sodales odio nisi nec massa. Proin quis neque nec sem finibus elementum. Praesent ultrices ante sit amet suscipit blandit. Praesent vulputate nibh est, vitae fringilla odio mattis eget. Aenean congue orci et leo placerat, nec semper ipsum convallis. Cras vestibulum volutpat lectus sed efficitur.";
 		Usuario carpinchote = new Usuario("Carpinchote");
-		Usuario carpinchi=new Usuario("Carpinchi");
-		Usuario carpancha=new Usuario("Carpancha");
+		Usuario carpinchi = new Usuario("Carpinchi");
+		Usuario carpancha = new Usuario("Carpancha");
 		carpinchote.añadirAmigo(carpinchi);
 		carpinchote.añadirAmigo(carpancha);
-		
+
 		usuarios.save(carpinchote);
 
 		Comentario caca = new Comentario("Vaya caca", carpinchote);
 
 		Serie cien = new Serie("Los 100", descripcion);
 		cien.getComentarios().add(caca);
-		Serie juegoTronos=new Serie("Juego de Tronos", descripcion);
+		Serie juegoTronos = new Serie("Juego de Tronos", descripcion);
 		series.save(cien);
 		series.save(new Serie("Breaking Bad", descripcion));
 		series.save(juegoTronos);
 		series.save(new Serie("Narcos", descripcion));
-		
+		series.save(new Serie("Westworld", descripcion));
+
 		carpinchote.añadirSerieFavorita(cien);
 		carpinchote.añadirSerieFavorita(juegoTronos);
 		usuarios.save(carpinchote);
-		
+
 	}
 
 	@RequestMapping("/")
-	public String indice(Model model) {
+	public String indice(Model model, @PageableDefault(value = 8) Pageable page) {
 
-		model.addAttribute("series", series.findAll());
+		Page<Serie> paginas = series.findAll(page);
+
+		String ordenar;
+		if (page.getSort() != null) {
+			ordenar = paginas.getSort().iterator().next().getProperty();
+		} else {
+			ordenar = "Sin orden";
+		}
+		model.addAttribute("ordenar", ordenar);
+		model.addAttribute("series", paginas);
+		model.addAttribute("siguiente", !paginas.isLast());
+		model.addAttribute("anterior", !paginas.isFirst());
+		model.addAttribute("actual", paginas.getNumber());
+		model.addAttribute("next", paginas.getNumber() + 1);
+		model.addAttribute("prev", paginas.getNumber() - 1);
 
 		return "index";
 	}
 
-	@RequestMapping("/{id}")
-	public String serie(Model model, @PathVariable long id) {
+	@RequestMapping("/{url}")
+	public String serie(Model model, @PathVariable String url) {
 
-		model.addAttribute("serie", series.findOne(id));
+		model.addAttribute("serie", series.findByUrl(url));
 
 		return "serie";
 	}
 
-	@RequestMapping("/{id}/comentarios")
-	public String comentarios(Model model, Comentario comentario, @PathVariable long id) {
+	@RequestMapping("/{url}/comentarios")
+	public String comentarios(Model model, Comentario comentario, @PathVariable String url) {
 
 		if (!comentario.esVacio()) {
-			Serie serie = series.findOne(id);
+			Serie serie = series.findByUrl(url);
 			comentario.setUsuario(usuarios.findOne((long) 1));
 			serie.getComentarios().add(comentario);
 			series.save(serie);
 		}
 
-		return "redirect:/{id}#comentarios";
+		return "redirect:/{url}#comentarios";
 
 	}
 
@@ -93,15 +111,16 @@ public class Inicio_Controller {
 	}
 
 	@RequestMapping("/perfil/{id}")
-	public String usuario(Model model,@PathVariable long id){
+	public String usuario(Model model, @PathVariable long id) {
 		model.addAttribute("usuario", usuarios.findOne(id));
 		return "perfil";
 	}
+
 	@RequestMapping("/perfil/{id}/editar")
-	public String editar(Model model,Usuario u,@PathVariable long id){
+	public String editar(Model model, Usuario u, @PathVariable long id) {
 
 		model.addAttribute("usuario", usuarios.findOne(id));
 		return "editar-perfil";
 	}
-	
+
 }
--- src/main/java/com/series/carpincho/SeriesRepository.java
+++ src/main/java/com/series/carpincho/SeriesRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeriesRepository extends JpaRepository<Serie, Long> {
 
+	Serie findByUrl(String url);
 }
