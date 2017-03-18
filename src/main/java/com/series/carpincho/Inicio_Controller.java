50883d079643d566a5b052690bd77a8c198115df
scgvk <sergio94.cg@gmail.com>
2017-03-18 20:12:42 +0100

    subir imagenes a fuegote

--- src/main/java/com/series/carpincho/Inicio_Controller.java
+++ src/main/java/com/series/carpincho/Inicio_Controller.java
@@ -1,10 +1,17 @@
 package com.series.carpincho;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,12 +19,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Inicio_Controller {
-
+	private static final Path FILES_FOLDER = Paths.get( "target/classes/static/img/perfil");
+	private List<String> images = new ArrayList<>();
+	
 	@Autowired
 	private SeriesRepository series;
 
@@ -278,4 +292,52 @@ public class Inicio_Controller {
 
 		return "redirect:/perfil";
 	}
+	@RequestMapping(value = "/perfil/editar", method = RequestMethod.POST)
+	public ModelAndView handleFileUpload(@RequestParam("imageTitle") String imageTitle,@RequestParam("file") MultipartFile file) {
+
+		if (userComponent.getLoggedUser() != null) {
+			Usuario usuario = usuarios.findByUser(userComponent.getLoggedUser().getUser());
+			String fileName = usuario.getUser() + ".png";
+			
+			if (!file.isEmpty()) {
+	
+				try {
+	
+					if (!Files.exists(FILES_FOLDER)) {
+						Files.createDirectories(FILES_FOLDER);
+					}
+	
+					File uploadedFile = new File(FILES_FOLDER.toAbsolutePath().toString(), fileName);
+					file.transferTo(uploadedFile);
+	
+					images.add(imageTitle);
+	
+					return new ModelAndView("editar-perfil").addObject("imageTitle", images);
+	
+				} catch (Exception e) {
+					e.printStackTrace();
+					return new ModelAndView("editar-perfil").addObject("fileName", fileName).addObject("error",e.getClass().getName() + ":" + e.getMessage());
+				}} else {
+					return new ModelAndView("editar-perfil").addObject("error", "The file is empty");
+				}}
+		return new ModelAndView("editar-perfil").addObject("imageTitle", images);
+		
+	}
+	
+	@RequestMapping("/perfil/editar/{fileName:.+}")
+	public void handleFileDownload(@PathVariable String fileName, HttpServletResponse res)
+			throws FileNotFoundException, IOException {
+
+		Path image = FILES_FOLDER.resolve(fileName);
+
+		if (Files.exists(image)) {
+			res.setContentType("image/png");
+			res.setContentLength((int) image.toFile().length());
+			FileCopyUtils.copy(Files.newInputStream(image), res.getOutputStream());
+
+		} else {
+			res.sendError(404, "File" + fileName + "(" + image.toAbsolutePath() + ") does not exist");
+		}
+	}
+
 }
