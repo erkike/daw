7a71d100c6824c412e1b306e4915a19a18de3221
erkike <e.sanchezpo@gmail.com>
2017-04-25 10:53:37 +0200

    mostrar series

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'app works!';
+  private series: string[] = [];
+
+  constructor(private http: Http){
+    this.series = [];
+
+    let url = "https://localhost:8443/series";
+
+    this.http.get(url).subscribe(
+      response => {
+        let data = response.json();
+        for (var i = 1; i < 21; i++){
+          let serie = data[i];
+          this.series.push(serie);
+        }
+      },
+      error => console.error(error)
+    );
+  }
 }
