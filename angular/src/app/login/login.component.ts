56f6a83138c322e2d0eae47f1526bd741a4e53bc
erkike <e.sanchezpo@gmail.com>
2017-04-30 20:07:37 +0200

    no se lo que hago bro

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {Router} from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
+import { Observable } from "rxjs/Observable";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'login.component.html'
+    selector: 'app-root',
+    templateUrl: 'login.component.html'
 })
 
-export class LoginComponent{
+export class LoginComponent {
 
-    constructor(){
+    user = 'user';
+    pass = 'pass';
 
-        let url = "https://localhost:8443/logIn";
+    private login = "https://localhost:8443/logIn";
+    constructor(private http: Http, private router: Router) {
+
+
+    }
+
+    registro(){}
+
+    autenticacion(user: string, pass: string): Observable<any> {
+
+        var headers = new Headers();
+        headers.append('Authorization', 'Basic ' + btoa(user+':'+pass));
+
+        return this.http.get(this.login, headers);
     }
 
 }
