96ec487847c1ad70b49a8ae394a912dcb8f615c4
erkike <e.sanchezpo@gmail.com>
2017-05-01 11:08:45 +0200

    registro funciona

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
 import { Observable } from "rxjs/Observable";
@@ -11,21 +11,36 @@ import { Observable } from "rxjs/Observable";
 
 export class LoginComponent {
 
-    user = 'user';
-    pass = 'pass';
+    nombre = '';
+    user = '';
+    email = '';
+    paswordHass = '';
+    userlog = '';
+    passlog = '';
 
     private login = "https://localhost:8443/logIn";
+    private registroUrl = "https://localhost:8443/usuarios";
+
     constructor(private http: Http, private router: Router) {
 
 
     }
 
-    registro(){}
+    registro() {
+
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        this.http.post(this.registroUrl, { nombre: this.nombre, user: this.user, email: this.email, passwordHass: this.paswordHass }, options).subscribe(
+            response => { this.router.navigate(['/home']); },
+            error => console.log(error)
+        );
+    }
 
     autenticacion(user: string, pass: string): Observable<any> {
 
         var headers = new Headers();
-        headers.append('Authorization', 'Basic ' + btoa(user+':'+pass));
+        headers.append('Authorization', 'Basic ' + btoa(user + ':' + pass));
 
         return this.http.get(this.login, headers);
     }
